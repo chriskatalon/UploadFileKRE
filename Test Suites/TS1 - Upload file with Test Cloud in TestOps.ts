41899ba48db0e40f437ce18cfb53b3c2c217b6e9
chriskatalon <99629253+chriskatalon@users.noreply.github.com>
2023-10-19 15:57:26 +0700

    a

--- Test Suites/TS1 - Upload file with Test Cloud in TestOps.ts
+++ Test Suites/TS1 - Upload file with Test Cloud in TestOps.ts
@@ -12,17 +12,17 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>9137814d-aabf-404e-a4ce-6ecd820991b7</testSuiteGuid>
    <testCaseLink>
-      <guid>9a8fc54a-77b8-4df5-89c1-55f42926e905</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Upload File - Copy</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>2dc3e002-ecb8-4fd4-9e73-86fffac01be9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Upload File</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9a8fc54a-77b8-4df5-89c1-55f42926e905</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Upload File with Custom script</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
